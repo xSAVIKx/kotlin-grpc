98113c1f92daa83b496d6a34e7b24020d467124c
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-04-28 13:03:37 +0300

    Use `Application` object in order to be able to pass the startup class reference.

--- client/src/main/kotlin/io/spine/client/Application.kt
+++ client/src/main/kotlin/io/spine/client/Application.kt
@@ -2,9 +2,12 @@ package io.spine.client
 
 import io.micronaut.runtime.Micronaut.build
 
-fun main(args: Array<String>) {
-    build()
-        .args(*args)
-        .packages("io.spine")
-        .start()
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        build()
+            .args(*args)
+            .packages("io.spine.client")
+            .start()
+    }
 }
--- server/src/main/kotlin/io/spine/server/Application.kt
+++ server/src/main/kotlin/io/spine/server/Application.kt
@@ -1,10 +1,13 @@
 package io.spine.server
 
-import io.micronaut.runtime.Micronaut.*
-fun main(args: Array<String>) {
-	build()
-	    .args(*args)
-		.packages("io.spine")
-		.start()
-}
+import io.micronaut.runtime.Micronaut.build
 
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        build()
+            .args(*args)
+            .packages("io.spine.server")
+            .start()
+    }
+}
