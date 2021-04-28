1d33aee773fcdb8930ff8820bd5ab929fc30df3e
Yuri Sergiichuk <yuri.sergiichuk@teamdev.com>
2021-04-28 13:37:28 +0300

    Implement all endpoints.

--- server/src/main/kotlin/io/spine/server/GreetingEndpoint.kt
+++ server/src/main/kotlin/io/spine/server/GreetingEndpoint.kt
@@ -5,7 +5,9 @@ import io.spine.model.HelloReply
 import io.spine.model.HelloRequest
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
 import javax.inject.Singleton
 
 @Singleton
@@ -29,10 +31,19 @@ class GreetingEndpoint : GreeterGrpcKt.GreeterCoroutineImplBase() {
     }
 
     override suspend fun clientTalking(requests: Flow<HelloRequest>): HelloReply {
-        return super.clientTalking(requests)
+        val clientRequests = requests.toList()
+        clientRequests.forEach { print(it) }
+        return HelloReply.newBuilder()
+            .setMessage("Received ${clientRequests.size} requests. Thx!")
+            .build()
     }
 
-    override fun talkingTogether(requests: Flow<HelloRequest>): Flow<HelloReply> {
-        return super.talkingTogether(requests)
+    override fun talkingTogether(requests: Flow<HelloRequest>): Flow<HelloReply> = flow {
+        requests.collect {
+            val reply = HelloReply.newBuilder()
+                .setMessage("Hello, ${it.name}.")
+                .build()
+            emit(reply)
+        }
     }
 }
